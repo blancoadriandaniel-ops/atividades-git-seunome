d70127cd546bfe47207d252fb94328a7bd40bcc8
Adrian Daniel Blanco Torrens <blancoadriandanielgmail.com>
2026-09-06 21:00:53 -0300

    feat: cria projeto AtividadesGit com a classe Principal

---
+++ AtividadesGit/src/main/java/atividades/Principal.java
@@ -0,0 +1,15 @@
+package atividades;
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Aluno: Seu Nome Completo");
+        System.out.println("Curso: Programacao Java");
+        System.out.println("Atividade de Git e GitHub");
+
+    }
+    
+}
---
+++ AtividadesGit/src/main/java/com/mycompany/atividadesgit/AtividadesGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividadesgit;
+
+/**
+ *
+ * @author 4ser
+ */
+public class AtividadesGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
